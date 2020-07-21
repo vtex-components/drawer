ced3e0b30581e7a731bc78b17b03f7918a529f34
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-07-20 22:29:48 -0300

    using theme-ui and reakit as peer dependency

--- stories/Default.stories.tsx
+++ stories/Default.stories.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { ThemeProvider } from 'theme-ui'
 
 import Drawer from '../src'
 
@@ -8,21 +7,4 @@ export default {
   component: Drawer,
 }
 
-const theme = {
-  fonts: {
-    body: 'system-ui, sans-serif',
-    heading: '"Avenir Next", sans-serif',
-    monospace: 'Menlo, monospace',
-  },
-  colors: {
-    text: '#000',
-    background: '#fff',
-    primary: '#33e',
-  },
-}
-
-export const Text = () => (
-  <ThemeProvider theme={theme}>
-    <Drawer />
-  </ThemeProvider>
-)
+export const Text = () => <Drawer />
